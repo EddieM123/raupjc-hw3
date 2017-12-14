c00fe5caaa7ae6bd0cfd23c3000723d4d621ccee
Edi <Edi@DESKTOP-1PRKBU7>
2017-12-15 00:59:08 +0100

    Add project files.

---
+++ TodoSqlRepository/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoSqlRepository
+{
+    public class Class1
+    {
+    }
+}
