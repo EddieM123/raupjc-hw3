c84cf3ad93dba9bc58b223ee3ad16240b29150b4
Edi <Edi@DESKTOP-1PRKBU7>
2017-12-18 00:03:46 +0100

    Pending Views and TodoController

--- WebAppTodo/Controllers/TodoController.cs
+++ WebAppTodo/Controllers/TodoController.cs
@@ -26,11 +26,11 @@ namespace WebAppTodo.Controllers
 
 
 
-        // GET: /todo/
+        // GET: /todo/index
         public async Task<IActionResult> Index()
         {
-            var todo = _repository.GetActive(new Guid((await _userManager.GetUserAsync(HttpContext.User)).Id));
             var viewModel = new IndexViewModel();
+            var todo = _repository.GetActive(await GetCurrentUserId());
 
             foreach (var td in todo)
             {
@@ -51,18 +51,45 @@ namespace WebAppTodo.Controllers
         {
             if (ModelState.IsValid)
             {
-                var todoItem = new TodoItem(model.Text, model.DateDue, new Guid((await _userManager.GetUserAsync(HttpContext.User)).Id));
+                var todoItem = new TodoItem(model.Text, model.DateDue, await GetCurrentUserId());
                 
                 _repository.Add(todoItem);
                 return RedirectToAction("Index");
             }
-
-            // If we got this far, something failed, redisplay form
+            
             return View(model);
         }
 
+        public async Task<IActionResult> Completed()
+        {
+            var viewModel = new CompletedViewModel();
+            var todos = _repository.GetCompleted(await GetCurrentUserId());
 
+            foreach (var todo in todos)
+            {
+                viewModel.TodoViewModels.Add(new TodoViewModel(todo));
+            }
 
+            return View(viewModel);
+        }
 
+        public async Task<IActionResult> MarkAsCompleted(Guid todoId)
+        {
+            var userId = await GetCurrentUserId();
+            _repository.MarkAsCompleted(todoId, userId);
+            return RedirectToAction("Index");
+        }
+
+       
+
+        /// <summary>
+        /// Gets Guid of the currently logged user. Think about pulling this helper method outside,
+        /// so other controllers can use it!
+        /// </summary>
+        private async Task<Guid> GetCurrentUserId()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            return new Guid(user.Id);
+        }
     }
 }
